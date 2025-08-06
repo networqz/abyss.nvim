8cd953ba9b7704f9ff32c386a8ad74a609fb3686
networqz <david.puncikar@gmail.com>
2025-08-06 21:31:46 +0200

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,29 @@
+-- editor
+vim.o.confirm = true -- raise a dialog on operations that would fail due to unsaved changes
+vim.o.scrolloff = 8 -- minimal number of screen lines above and belove sursor
+vim.o.signcolumn = "yes" --- set signcolumn to be always visible
+
+vim.o.updatetime = 128 -- time to wait for swap file update in ms
+vim.o.timeoutlen = 256 -- time to wait for mapped sequence in ms
+
+-- leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- cursor
+vim.o.guicursor = "" -- set cursor to block in all modes
+vim.o.cursorline = true -- highlight line where is cursor
+
+-- numbers
+vim.o.nu = true -- enable line numbers
+vim.o.relativenumber = true -- enable relative numbers for easier jumping
+
+-- tab
+vim.o.tabstop = 4 -- spaces
+vim.o.softtabstop = 4 -- spaces
+vim.o.shiftwidth = 4 -- spaces
+vim.o.expandtab = true -- use spaces instead of tabs
+
+-- search
+vim.o.hlsearch = false
+vim.o.incsearch = true
